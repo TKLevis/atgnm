a2dfce7effd4a329aadb655a3a3a5b614a528648
Felix Windsheimer <felix.windsheimer@fau.de>
2023-02-05 20:25:58 +0100

    remove unneeded old code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import "./index.css";
 import { Suspense, useEffect, useState } from "react";
-import { Button, Col } from "react-bootstrap";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, OrbitControls, Environment, useGLTF } from "@react-three/drei";
 
 import ReliefModel from './ReliefGNM3.glb'
 
-import black from './images/black.png'
-import RekonstruktionFoto from './images/RekonstruktionFoto.jpg'
 import RekonstruktionFotoRelief from './images/RekonstruktionFotoRelief.jpg'
+import RekonstruktionFotoReliefKoepfe from './images/RekonstruktionFotoReliefKoepfe.jpg'
 import RekonstruktionFotoSchrift from './images/RekonstruktionFotoSchrift.jpg'
 
 //RELIEF
 function Relief({
   highlightTuch,
   setHighlightTuch,
-  highlightJesus,
-  setHighlightJesus,
   highlightHeads,
   highlightBroken,
-  setInfoText,
-  canHighlight,
   cameraPosition,
   cameraRotation,
 }) {
   const { nodes, materials } = useGLTF(ReliefModel);
-
-  var currentMatTuch = materials.material0;
-  var currentMatJesus = materials.material0;
-
-  //Check Conditions for Highlighting different parts of the mesh with the highlight material(materials.hoverJesus works for every part of the mesh, don't be confused by the name)
-  if (highlightTuch === true && canHighlight) {
-    currentMatTuch = materials.HoverJesus;
-  } else {
-    currentMatTuch = materials.material0;
-  }
-
-  if (highlightJesus === true && canHighlight) {
-    currentMatJesus = materials.HoverJesus;
-  } else {
-    currentMatJesus = materials.material0;
-  }
 
   return (
     <group dispose={null} rotation={[2.4, -0.05, -0.05]}>
@@ -53,24 +31,12 @@ function Relief({
           // TODO delete?
           onPointerEnter={() => setHighlightTuch(true)}
           onPointerLeave={() => setHighlightTuch(false)}
-          onClick={() => {
-            setInfoText(
-              "Das Schweisstuch, in das sich das Gesicht Jesu eingedrückt hat. Vera Ikon."
-            );
-          }}
           geometry={nodes.Model_2.geometry}
           material={highlightTuch ? materials.HoverJesus : materials.material0}
         />
         <mesh
-          onPointerEnter={() => setHighlightJesus(true)}
-          onPointerLeave={() => setHighlightJesus(false)}
-          onClick={() =>
-            setInfoText(
-              "Dies ist Jesus. Er wurde vor 2000 Jahren geboren und wird von manchen als der Messiahs angesehen. War sicher ein cooler Dude."
-            )
-          }
           geometry={nodes.Model_3.geometry}
-          material={highlightHeads || highlightJesus ? materials.HoverJesus : materials.material0}
+          material={highlightHeads ? materials.HoverJesus : materials.material0}
         />
         <mesh
           geometry={nodes.Model_4.geometry}
@@ -395,223 +361,14 @@ function QuizUI({
   );
 }
 
-//TEXT UI unterhalb des 3D Modells | Unterteilt in 3 States die über Buttons geändert werden (Besser wären Radiobuttons)
-function TextUI(props) {
-  const [lernState, setLernState] = [props.lernState, props.setLernState];
-  const [canHighlight, setCanHighlight] = [
-    props.canHighlight,
-    props.setCanHighlight,
-  ];
-  const [infoText, setInfoText] = [props.infoText, props.setInfoText];
-  const [highlightTuch, setHighlightTuch] = [
-    props.highlightTuch,
-    props.setHighlightTuch,
-  ];
-  const [highlightJesus, setHighlightJesus] = [
-    props.highlightJesus,
-    props.setHighlightJesus,
-  ];
-  const [fragen, setFragen] = [props.fragen, props.setFragen];
-  const [questionCounter, setQuestionCounter] = [
-    props.questionCounter,
-    props.setQuestionCounter,
-  ];
-
-  //LERNMODUS
-  if (lernState === "Lernmodus") {
-    setCanHighlight(true);
-    console.log(lernState);
-    return (
-      <div>
-        <h2>Lernmodus: Relief aus dem Germanischen Nationalmuseum</h2>
-        <h3>Info: {infoText}</h3>
-        <div>
-          <Col className="button-column">
-            <Button
-              type="button"
-              className="button1"
-              onClick={() => setLernState("Quizmodus")}
-            >
-              {" "}
-              Quizmodus{" "}
-            </Button>
-            <Button
-              type="button"
-              className="button1"
-              onClick={() => setLernState("Anschaumodus")}
-            >
-              {" "}
-              Anschaumodus{" "}
-            </Button>
-          </Col>
-        </div>
-      </div>
-    );
-  }
-
-  //QUIZMODUS
-  //aktuell ist der Jesus highlight hardcoded -> todo: durch json steuern
-  //aktuell muss hier für jedes mesh individuell highlight=false in den button gesetzt werden
-  else if (lernState === "Quizmodus") {
-    setCanHighlight(true);
-    setHighlightJesus(true);
-    console.log(lernState + ", Frage 1");
-    return (
-      <div>
-        <h2>Quizmodus: Relief aus dem Germanischen Nationalmuseum</h2>
-        <InputField
-          fragen={fragen}
-          setFragen={setFragen}
-          questionCounter={questionCounter}
-          setQuestionCounter={setQuestionCounter}
-        />
-        <div>
-          <Col className="button-column">
-            <Button
-              className="button1"
-              type="button"
-              onClick={() => {
-                setHighlightJesus(false);
-                setHighlightTuch(false);
-                setInfoText(
-                  "Klick auf einen hervorgehobenen Bereich um mehr zu lernen!"
-                );
-                setLernState("Lernmodus");
-                setQuestionCounter(0);
-              }}
-            >
-              {" "}
-              Lernmodus{" "}
-            </Button>
-            <Button
-              className="button1"
-              type="button"
-              onClick={() => {
-                setLernState("Anschaumodus");
-                setQuestionCounter(0);
-              }}
-            >
-              {" "}
-              Anschaumodus{" "}
-            </Button>
-          </Col>
-        </div>
-      </div>
-    );
-  }
-
-  //ANSCHAUMODUS
-  else if (lernState === "Anschaumodus") {
-    setCanHighlight(false);
-
-    return (
-      <div>
-        <h2>Anschaumodus: Relief aus dem Germanischen Nationalmuseum</h2>
-        <div>
-          <Col className="button-column">
-            <Button
-              type="button"
-              className="button1"
-              onClick={() => {
-                setHighlightJesus(false);
-                setHighlightTuch(false);
-                setInfoText(
-                  "Klick auf einen hervorgehobenen Bereich um mehr zu lernen!"
-                );
-                setLernState("Lernmodus");
-              }}
-            >
-              {" "}
-              Lernmodus{" "}
-            </Button>
-            <Button
-              type="button"
-              className="button1"
-              onClick={() => setLernState("Quizmodus")}
-            >
-              {" "}
-              Quizmodus{" "}
-            </Button>
-          </Col>
-        </div>
-      </div>
-    );
-  }
-}
-
-//INPUTFIELD FÜR QUIZ
-function InputField(props) {
-  const [questionCounter, setQuestionCounter] = [
-    props.questionCounter,
-    props.setQuestionCounter,
-  ];
-  const [antwortText, setAntwortText] = useState("");
-  const fragen = props.fragen.fragen;
-  const frage = fragen[questionCounter].frage;
-  const richtigeAntworten = fragen[questionCounter].richtigeAntworten;
-
-  function handleAntwortChange(event) {
-    console.log(this);
-    const { value } = event.target;
-    setAntwortText(value);
-  }
-
-  function afterSubmission() {
-    if (richtigeAntworten.includes(antwortText)) {
-      if (questionCounter < fragen.length - 1) {
-        alert("Richtig!");
-        console.log(questionCounter + " < " + fragen.length);
-        console.log("Warum TU ICH DAS????");
-        setQuestionCounter(questionCounter + 1);
-      } else {
-        alert("Richtig! Das war die letzte Frage. Bitte beende den Quizmodus");
-      }
-    } else {
-      alert("Falsch!");
-    }
-  }
-
-  return (
-    <div>
-      <h3> Frage: {frage} </h3>
-      <form>
-        <label>
-          {" "}
-          Antwort:
-          <input
-            onChange={handleAntwortChange}
-            type="text"
-            name="antwortText"
-            value={antwortText}
-          />
-        </label>
-        <input
-          type="button"
-          onClick={afterSubmission}
-          value=">"
-          style={{ fontSize: "20px", fontWeight: "bold" }}
-        />
-      </form>
-    </div>
-  );
-}
-
 // App-Component nutzt den State-Hook von react. useState returns current state & function that updates it
 // App-Component passes state and update-function down to child-components (z.B. TextUI, Relief)
 // -> state kann von überall geupdated und gelesen werden über props die der Component von parent gegeben werden
 // !!!Schlecht skalierbar: für neue highlightable meshes muss ein state + update-function definiert werden, und zu Relief & TextUI gepasst werden, und dort updates callen
 function App() {
-  const [infoText, setInfoText] = useState(
-    "Klick auf einen hervorgehobenen Bereich um mehr zu lernen!"
-  );
-  const [lernState, setLernState] = useState("Lernmodus");
   const [highlightTuch, setHighlightTuch] = useState(false);
-  const [highlightJesus, setHighlightJesus] = useState(false);
   const [highlightHeads, setHighlightHeads] = useState(false);
   const [highlightBroken, setHighlightBroken] = useState(false);
-  const [canHighlight, setCanHighlight] = useState(true);
-  const [fragen, setFragen] = useState([]);
-  const [questionCounter, setQuestionCounter] = useState(0);
 
   const cameraPositionStart = [6, 5, 13];
   const cameraRotationStart = [0.0, 0.05, 0.0];
@@ -619,16 +376,9 @@ function App() {
   const [cameraRotation, setCameraRotation] = useState(cameraRotationStart);
   const [image, setImage] = useState(null);
 
-  // FRAGEN LADEN VON JSON
-  useEffect(() => {
-    fetch("/Quiz.json")
-      .then((response) => response.json())
-      .then((data) => setFragen(data));
-  }, []);
-
   // TODO initially show that model is movable?
 
-  //Hier werden alle JSX-Elemente(ähnlich html) und Components(z.B. Relief und TextUI) zusammengeführt die in index.js gerendert werden
+  // Hier werden alle JSX-Elemente(ähnlich html) und Components(z.B. Relief und TextUI) zusammengeführt die in index.js gerendert werden
   // außerdem werden den child-components state & update-function als props gegeben
   return (
     <div className="wrapper">
@@ -638,14 +388,8 @@ function App() {
             <Canvas>
               <Environment preset="warehouse" background blur={0.6}/>
               <Relief
-                infoText={infoText}
-                setInfoText={setInfoText}
-                canHighlight={canHighlight}
-                setCanHighlight={setCanHighlight}
                 highlightTuch={highlightTuch}
                 setHighlightTuch={setHighlightTuch}
-                highlightJesus={highlightJesus}
-                setHighlightJesus={setHighlightJesus}
                 highlightHeads={highlightHeads}
                 highlightBroken={highlightBroken}
                 cameraPosition={cameraPosition}
